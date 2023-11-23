467f40859438c50ec15b6dce53de470532c14d34
Lkaua22k <lkauan462@gmail.com>
2023-11-23 13:35:08 -0300

    Criando a pagina de profile :tada:

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,13 @@ import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './Styles/global.js'
 import theme from './Styles/theme.js'
 
-import {SignUp} from './pages/SignUp'
+import {Profile} from './pages/Profile'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SignUp />
+      <Profile />
     </ThemeProvider>
   </React.StrictMode>
 )
---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,12 @@
+import {FiArrowLeft} from "react-icons/fi"
+import { Container } from "./style";
+
+export function Profile(){
+    return(
+        <Container>
+            <header>
+                <a href=""> Voltar</a>
+            </header>
+        </Container>
+    )
+}
